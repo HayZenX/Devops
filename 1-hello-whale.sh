df0003872b33d0203c0d9807fe91d0282bebd194
HayZenX <simon.pruvost2004@gmail.com>
2025-10-20 05:55:09 -0400

    Edit for add cowsay

--- 1-hello-whale.sh
+++ 1-hello-whale.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Ce script exécute un conteneur whalesay et l’efface après exécution
 
-docker run --rm anthonyjhoiro/whalesay "Hello M1 Cyber 2025"
+docker run --rm anthonyjhoiro/whalesay cowsay "Hello M1 Cyber 2025"
